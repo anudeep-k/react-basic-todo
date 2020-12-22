17ff53e5ab3bc226e85cdbcfc0e92d60a7238275
anudeep-k <36880670+anudeep-k@users.noreply.github.com>
2020-12-22 15:28:14 +0530

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Todo from './components/Todo';
+import Todolist from './components/Todolist';
+
+function App() {
+  return (
+    <div className="App">
+    
+<Todolist />
+    
+    </div>
+  );
+}
+
+export default App;
